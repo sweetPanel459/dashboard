71d5a7e3492675727f4f5c92a886f666144ac723
otpJs <sssantiago459@gmail.com>
2024-09-19 19:54:06 -0500

    feat: creation of table rows

--- src/containers/tableSections.jsx
+++ src/containers/tableSections.jsx
@@ -1,3 +1,4 @@
+import { Text } from "../components/common/texts";
 import { Button } from "../components/common/button";
 
 import {
@@ -5,6 +6,7 @@ import {
    FaSortAmountDownAlt,
    FaTrashAlt,
    FaPencilAlt,
+   FaPlus,
 } from "react-icons/fa";
 
 const optionHeadExample = [
@@ -18,15 +20,15 @@ const optionHeadExample = [
 ];
 
 export const TableHeader = () => (
-   <tr className="flex w-full gap-2">
+   <tr className="flex w-full gap-2 ">
       {optionHeadExample.map((index, keys) => (
          <td className="w-full">
             <Button
                key={keys}
                text={index.text}
                icon={index.click && <FaSortAmountDown />}
-               styleIcon="order-2"
-               styleButton="items-center w-full gap-3 h-full text-lg font-montserrat font-bold text-gray-700 hover:text-gray-900"
+               styleIcon="order-2 relative bottom-0.5 text-xl"
+               styleButton="items-center justify-center w-full gap-3 h-full text-xl font-montserrat font-bold text-gray-700 hover:text-gray-900"
             />
          </td>
       ))}
@@ -34,32 +36,38 @@ export const TableHeader = () => (
    </tr>
 );
 
-export const TableRow = () => (
+export const TableRow = ({ cellValues }) => (
    <tr className="flex items-center w-full gap-2 py-3 border-b border-gray-300">
-      <td className="text-lg font-semibold w-full">Juan santiago</td>
-      <td className=" w-full">1</td>
-      <td className=" w-full">3</td>
-      <td className=" w-full">1233</td>
-      <td className=" w-full">123</td>
-      <td className=" w-full">12</td>
-      <td className=" w-full">10</td>
+      {cellValues.map((index, key) => (
+         <td
+            key={key}
+            className="relative right-1 text-center w-full font-grotesk font-edium"
+         >
+            {index}
+         </td>
+      ))}
       <EditRow />
    </tr>
 );
 
 const EditRow = ({ state = true }) => (
-   <td className="flex items-center justify-center gap-2 h-8 min-w-20">
-      {state && (
+   <td className="flex items-center justify-center gap-2 h-7 min-w-20">
+      {state ? (
          <>
             <Button
                icon={<FaPencilAlt />}
-               styleButton="flex-grow items-center justify-center h-full rounded text-white bg-gray-500"
+               styleButton="flex-grow items-center justify-center h-full rounded text-white bg-blue-400 hover:bg-blue-500"
             />
             <Button
                icon={<FaTrashAlt />}
-               styleButton="flex-grow items-center justify-center h-full rounded text-white bg-amber-500"
+               styleButton="flex-grow items-center justify-center h-full rounded text-white bg-red-400 hover:bg-red-500"
             />
          </>
+      ) : (
+         <Button
+            icon={<FaPlus />}
+            styleButton="items-center justify-center h-full px-2 rounded text-white bg-blue-400 hover:bg-blue-500"
+         />
       )}
    </td>
 );
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -8,12 +8,36 @@ import { FaSearch } from "react-icons/fa";
 
 import "../styles/table.css";
 
+const exampleData = [
+   ["TATIANA CAROLINA", 1, 1, 1, 1, 4, 1],
+   ["YEIMI ATENCIO", 2, 4, 1, 3, 10, 2],
+   ["ALEJANDRA MARTINEZ", 7, 2, 2, 3, 14, 3],
+   ["YURANIS PACHECO", 5, 3, 12, 4, 24, 4],
+   ["LINA DOVAL", 8, 6, 4, 3, 12, 5],
+   ["LINA GARCIA", 6, 10, 5, 5, 26, 6],
+   ["JHANA ARROYO", 9, 9, 9, 9, 34, 7],
+   ["CRISTINA CARDONA", 12, 11, 10, 5, 38, 8],
+   ["GRACE NARANJO", 14, 5, 14, 5, 40, 9],
+   ["LORENA FUENTES", 20, 5, 15, 11, 43, 10],
+   ["MARIA ALEJANDRA", 15, 14, 7, 11, 47, 11],
+   ["LILIANA NEGRETE", 13, 13, 12, 11, 49, 12],
+   ["DAYANA ARGEL", 13, 12, 13, 11, 50, 13],
+   ["DAYSSI PARRA", 17, 16, 11, 8, 51, 14],
+   ["LEIDY SANCHEZ", 4, 17, 16, 15, 52, 15],
+   ["WENDY AYAZO", 3, 20, 12, 17, 52, 15],
+   ["ISABELLA LOZANO", 21, 12, 17, 16, 59, 17],
+   ["DAIRYS FURNIELES", 15, 21, 15, 20, 61, 18],
+   ["MARIED BLANQUICET", 15, 21, 18, 16, 73, 19],
+   ["DINA TEHERAN", 19, 18, 16, 16, 73, 19],
+   ["MARIA FERNANDA COL", 18, 19, 21, 18, 76, 21],
+];
+
 export const DashBoard = () => {
    return (
-      <section className="flex flex-col gap-10 w-screen h-screen px-60 bg-gray-50 overflow-y-auto">
+      <section className="flex flex-col gap-10 w-screen h-screen px-52 bg-gray-50 overflow-y-auto">
          <DashboardHeader />
 
-         <div className="flex flex-col gap-8 w-full h-screen">
+         <div className="flex flex-col gap-8 w-full h-screen pb-32">
             <header className="flex justify-between items-center w-full pb-5 border-b border-gray-200">
                <Select
                   placeholder="Mostrar..."
@@ -22,7 +46,7 @@ export const DashBoard = () => {
                <Input
                   button={<FaSearch />}
                   placeholder="Buscar..."
-                  styleButton="flex justify-center items-center order-2 h-full w-10 rounded bg-amber-500 text-white"
+                  styleButton="flex justify-center items-center order-2 h-full w-10 rounded bg-blue-400 text-white"
                   styleContainer="flex items-center gap-2 h-full p-1 pl-2 rounded border border-gray-500 bg-white"
                />
             </header>
@@ -33,7 +57,9 @@ export const DashBoard = () => {
                </thead>
 
                <tbody className="flex flex-col gap-2">
-                  <TableRow />
+                  {exampleData.map((index, key) => (
+                     <TableRow key={key} cellValues={index} />
+                  ))}
                </tbody>
             </table>
          </div>
