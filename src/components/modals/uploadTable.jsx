64debf415ef86c23f5c26eb6cd6f73a875235c4c
otpJs <sssantiago459@gmail.com>
2024-10-12 23:42:10 -0500

    feat: Creation of the logic to upload the excel

--- src/components/modals/uploadTable.jsx
+++ src/components/modals/uploadTable.jsx
@@ -1,21 +1,206 @@
-import { Input } from "../common/input";
-import { DoubleText, Text, Title } from "../common/texts";
+import { read, utils } from "xlsx";
 
+import { Input } from "../common/input";
+import { DoubleText } from "../common/texts";
+
+import { TableRow } from "../../containers/tableSections";
 import { ModalHeader } from "./modalHeader";
 
-import { FaTableList } from "react-icons/fa6";
 import { FaFileUpload } from "react-icons/fa";
+
+const worksheetExample = [
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    "ID",
+    "Producto",
+    "Cantidad",
+    "Precio",
+    null,
+    null,
+    "ID",
+    "Producto",
+    "Cantidad",
+    "Precio",
+    null,
+    null,
+    "ID",
+    "Producto",
+    "Cantidad",
+    "Precio",
+    null,
+    null,
+  ],
+  [
+    null,
+    1,
+    "Manzanas",
+    50,
+    0.5,
+    null,
+    null,
+    1,
+    "Manzanas",
+    50,
+    0.5,
+    null,
+    null,
+    1,
+    "Manzanas",
+    50,
+    0.5,
+    null,
+    null,
+  ],
+  [
+    null,
+    2,
+    "Peras",
+    30,
+    0.7,
+    null,
+    null,
+    2,
+    "Peras",
+    30,
+    0.7,
+    null,
+    null,
+    2,
+    "Peras",
+    30,
+    0.7,
+    null,
+    null,
+  ],
+  [
+    null,
+    3,
+    "Plátanos",
+    null,
+    0.3,
+    null,
+    null,
+    3,
+    "Plátanos",
+    null,
+    0.3,
+    null,
+    null,
+    3,
+    "Plátanos",
+    null,
+    0.3,
+    null,
+    null,
+  ],
+  [
+    null,
+    4,
+    "Naranjas",
+    20,
+    undefined,
+    null,
+    null,
+    4,
+    "Naranjas",
+    20,
+    undefined,
+    null,
+    null,
+    null,
+    4,
+    "Naranjas",
+    20,
+    undefined,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+];
+
+const ReadFileAsDataUrl = (file) => {
+  return new Promise((res, rej) => {
+    const reader = new FileReader();
+
+    reader.onload = (e) => res(e.target.result.split(",")[1]);
+    reader.onerror = (error) =>
+      rej(new Error({ msg: "no se pudo cargar el archivo", err: error }));
+
+    reader.readAsDataURL(file);
+  });
+};
+
+const uploadFileHandler = async (e) => {
+  const file = e.target.files[0];
+
+  const fileBase64 = await ReadFileAsDataUrl(file);
+  const workbook = read(fileBase64, { type: "base64" });
+
+  const worksheets = {};
+
+  workbook.SheetNames.forEach((sheetName) => {
+    const sheet = workbook.Sheets[sheetName];
+    worksheets[sheetName] = utils.sheet_to_json(sheet, { header: 1 });
+  });
+
+  console.log(worksheets);
+};
 
 export const UploadTable = ({ modalRef, close }) => {
   return (
     <div
       ref={modalRef}
-      className="uploadTable absolute inset-0 hidden items-center justify-center bg-transparent backdrop-blur"
+      className="uploadTable absolute inset-0 flex items-center justify-center bg-transparent backdrop-blur"
     >
-      <section className="modal flex flex-col gap-5 w-3/5 h-2/3 p-5 rounded-lg border border-gray-500 bg-white">
+      <section className="flex flex-col gap-5 w-4/5 h-4/5 p-5 rounded-lg border border-gray-500 bg-white">
         <ModalHeader text="Subir tabla" click={close} />
-        <form className="flex flex-grow gap-5 w-full">
-          <section className="flex flex-col gap-5 w-full h-full ">
+        <form className="flex gap-5 h-full w-full">
+          <section className="flex flex-col gap-5 w-1/5 h-full ">
             <Input
               id="upload-file"
               type="file"
@@ -23,25 +208,29 @@ export const UploadTable = ({ modalRef, close }) => {
               styleInput="hidden"
               styleLabel="flex items-center justify-center w-full h-full"
               styleContainer="flex-grow w-full rounded-lg border-2 border-dashed border-black"
+              changeInput={uploadFileHandler}
             />
-            <div className="flex justify-between gap-5 w-full h-10">
-              <input
-                type="text"
-                placeholder="Nombre de la tabla"
-                className=" flex-grow h-full border-b-2 outline-none border-gray-600"
-              />
-              <label
-                htmlFor="upload-file"
-                className="flex items-center justify-center flex-grow h-full rounded-lg  text-lg font-semibold bg-gray-600 text-white"
-              >
-                Subir archivo
-              </label>
-            </div>
+            <Input
+              type="text"
+              placeholder="Nombre de la tabla"
+              styleInput="bg-transparent"
+              styleContainer="h-fit py-1 border-b-2 outline-none border-gray-600"
+            />
           </section>
 
-          <section className="flex flex-col gap-4 w-full h-full">
-            <Title text="Tablas obtenidas del archivos:" style="text-2xl" />
-            <TableCardObtained />
+          <section className="flex flex-grow h-full overflow-auto">
+            <table className="w-full h-full border border-gray-400">
+              {worksheetExample.map((index, key) => {
+                return (
+                  <TableRow
+                    key={key}
+                    style="h-20"
+                    styleColm="w-32 pt-2 h-full border border-gray-400"
+                    cellValues={index}
+                  />
+                );
+              })}
+            </table>
           </section>
         </form>
       </section>
@@ -59,12 +248,5 @@ const ContentLabel = () => (
       fristTextStyle="text-gray-600 font-medium"
       secondTextStyle="text-gray-400"
     />
-  </div>
-);
-
-const TableCardObtained = () => (
-  <div className="flex gap-2 p-2 w-full rounded-lg border-2 border-gray-600">
-    <FaTableList className="text-2xl" />
-    <Text text="Nombre de la tabla" />
   </div>
 );
