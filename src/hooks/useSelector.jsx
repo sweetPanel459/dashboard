7cf0bd6c08a5c473e9b9ee0154076ccfd08ef06c
otpJs <sssantiago459@gmail.com>
2024-10-29 10:06:19 -0500

    fix: null value within rowRange

--- src/hooks/useSelector.jsx
+++ src/hooks/useSelector.jsx
@@ -7,12 +7,15 @@ const initialState = {
 
 export const useSelector = (workSheet) => {
   const [table, setTable] = useState([]);
-  const [shiftPressed, setShiftPressed] = useState(false);
 
+  const [shiftPressed, setShiftPressed] = useState(false);
   const [rowRange, setRowRange] = useState(initialState);
   const [columnRange, setColumnRange] = useState(initialState);
 
   const tableRef = useRef(null);
+
+  const hasValue = (property) =>
+    rowRange[property] == null && columnRange[property] == null ? true : false;
 
   const shiftDown = (e) => {
     if (e.key == "Shift") setShiftPressed(true);
@@ -21,22 +24,21 @@ export const useSelector = (workSheet) => {
   const shiftUp = (e) => {
     if (e.key == "Shift") {
       setShiftPressed(false);
-      setColumnRange(initialState);
       setRowRange(initialState);
+      setColumnRange(initialState);
     }
   };
 
   const clickTable = (e) => {
     if (!e.target.classList.contains("box-table") || !shiftPressed) return;
 
+    console.log("matame");
+
     const box = e.target.id.split(",");
     const boxIndex = { rowId: box[0], columnId: box[1] };
 
-    if (rowRange.initialIndex == null && columnRange.initialIndex == null) {
-      initialIndexTable(boxIndex);
-    } else {
-      finalIndexTable(boxIndex);
-    }
+    if (hasValue("initialIndex")) initialIndexTable(boxIndex);
+    else finalIndexTable(boxIndex);
   };
 
   const initialIndexTable = (index) => {
@@ -47,6 +49,15 @@ export const useSelector = (workSheet) => {
   const finalIndexTable = (index) => {
     setColumnRange((prev) => ({ ...prev, finalIndex: index.columnId }));
     setRowRange((prev) => ({ ...prev, finalIndex: index.rowId }));
+
+    console.log(rowRange.initialIndex, rowRange.finalIndex);
+    if (hasValue("finalIndex")) createTable();
+  };
+
+  const createTable = () => {
+    const selectedSectionTable = [];
+
+    // console.log(workSheet.slice(rowRange.initialIndex, rowRange.finalIndex));
   };
 
   const registerNodeTable = useCallback((node) => {
